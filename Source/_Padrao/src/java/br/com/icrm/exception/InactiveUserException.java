f93b12f69d7f0aa3ab630cede748377fc1142ee7
Reinaldo Oliveira <reinaldooli@gmail.com>
2012-03-19 16:33:33 -0300

    Removendo Deletados

--- Source/_Padrao/src/java/br/com/icrm/exception/InactiveUserException.java
+++
@@ -1,8 +0,0 @@
-package br.com.icrm.exception;
-
-public class InactiveUserException extends SecurityException {
-
-    public InactiveUserException() {
-        super("O Usuário informado está Inativo.");
-    }
-}
--- Source/_Padrao/src/java/br/com/icrm/exception/LoginException.java
+++
@@ -1,8 +0,0 @@
-package br.com.icrm.exception;
-
-public class LoginException extends SecurityException {
-
-    public LoginException() {
-        super("Usuário e/ou Senha inválidos!");
-    }
-}
